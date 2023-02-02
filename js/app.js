64b802c13b11d22f0f91197131335169197bcff6
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2023-02-02 19:39:55 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,30 +14,45 @@ window.onload = function () {
             service[1].disabled = true;
             service[2].disabled = true;
             service[3].disabled = true;
+          
+        
+            document.getElementById("href").scrollIntoView({ block: "center" });
+
+            if (isNaN(current)) {
+
+                current = x;
+
+                aside[x].classList = "animation";
+
+                setTimeout(function () {
+
+                    service[0].disabled = false;
+                    service[1].disabled = false;
+                    service[2].disabled = false;
+                    service[3].disabled = false;
+                    el.focus()
+                }, 1000)
+
+                return
+            }
+
             setTimeout(function () {
+
                 service[0].disabled = false;
                 service[1].disabled = false;
                 service[2].disabled = false;
                 service[3].disabled = false;
                 el.focus()
-            }, 1500)
-        
-            document.getElementById("href").scrollIntoView({ block: "center" });
-
-            if (isNaN(current)) {
-                current = x;
-                aside[x].classList = "animation";
-                return
-            }
+            }, 2000);
 
             aside[current].classList = "animationout";
+
             current = x;
+
             setTimeout(function () {
 
                 aside[x].classList = "animation";
-            }, 1000)
-
-
+            }, 1000);
         });
     })
 }
