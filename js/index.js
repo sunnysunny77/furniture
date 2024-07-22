63c677d904d7522aeb586d3e7cd4f1d92e221ac0
daniel <contractor@lateralaspect.com.au>
2024-07-22 12:53:42 +0800

    up

---
+++ js/index.js
@@ -0,0 +1,23 @@
+import { events } from "./utillites.js";
+import { slider_6 } from "./slider_6.js";
+
+// import '../node_modules/bootstrap/js/dist/alert';
+// import '../node_modules/bootstrap/js/dist/button';
+import '../node_modules/bootstrap/js/dist/carousel';
+// import '../node_modules/bootstrap/js/dist/collapse';
+// import '../node_modules/bootstrap/js/dist/dropdown';
+// import '../node_modules/ootstrap/js/dist/modal';
+// import '../node_modules/bootstrap/js/dist/offcanvas';
+// import '../node_modules/bootstrap/js/dist/popover';
+// import '../node_modules/bootstrap/js/dist/scrollspy';
+// import '../node_modules/bootstrap/js/dist/tab';
+// import '../node_modules/bootstrap/js/dist/toast';
+// import '../node_modules/bootstrap/js/dist/tooltip';
+import "../node_modules/@fortawesome/fontawesome-free/js/all.js";
+// import { service_worker } from "./service_worker.js";
+
+events(window, "load", () => {
+
+  slider_6();
+  //service_worker();
+});
