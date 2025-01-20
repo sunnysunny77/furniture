f165677918ee89705d84fb91f6ac1be24667c81c
daniel <contractor@lateralaspect.com.au>
2025-01-20 17:41:30 +0800

    up

--- js/slider_6.js
+++ js/slider_6.js
@@ -21,46 +21,46 @@ export const slider_6 = () => {
     if (count === 0) {
         clearInterval(inter_id);
         action_aside[previous].style.transition =
-        "opacity 0.5s ease-out";
+        "opacity 0.5s cubic-bezier(0,.67,.357,1)";
         action_aside[previous].classList.remove("has-animation");
         setTimeout(() => {
-        action_aside[previous].style.transition = "none";
-        }, 750);
+            action_aside[previous].style.transition = "none";
+        }, 500);
     }
     };
 
     for (const [i, index] of button_group.entries()) {
         events(index, "click", function() {
-        count = 12;
+        count = 9;
         clearInterval(inter_id);
         inter_id = setInterval(interval, 1000);
         disabled(true);
         setTimeout(() => {
             disabled(false);
-        }, 1500);
+        }, 500);
         if (previous === undefined) {
             previous = i;
             action_aside[i].style.transition =
-            "opacity 0.5s ease-out";
+            "opacity 0.5s cubic-bezier(0,.67,.357,1)";
             action_aside[i].classList.add("has-animation");
             setTimeout(() => {
             action_aside[i].style.transition = "none";
-            }, 750);
+            }, 500);
             return;
         }
         action_aside[previous].style.transition =
-            "opacity 0.5s ease-out";
+            "opacity 0.5s cubic-bezier(0,.67,.357,1)";
         action_aside[previous].classList.remove("has-animation");
         setTimeout(() => {
             action_aside[previous].style.transition = "none";
             action_aside[i].style.transition =
-            "opacity 0.5s ease-out";
+            "opacity 0.5s cubic-bezier(0,.67,.357,1)";
             action_aside[i].classList.add("has-animation");
             setTimeout(() => {
             action_aside[i].style.transition = "none";
             previous = i;
-            }, 750);
-        }, 750);
+            }, 500);
+        }, 500);
         });
     }
 };
